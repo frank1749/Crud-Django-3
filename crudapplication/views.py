0d704651b7852ded5937292230b10ece8c9b94c1
Frank Murillo <frank1749@hotmail.com>
2019-03-12 11:01:02 -0500

    Init Commit

---
+++ crudapplication/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from crudapplication.forms import EmployeeForm
+from crudapplication.models import Employee
+
+def home(request):  
+    return render(request, "home.html") 
+
+def emp(request):
+    if request.method == "POST":
+       form = EmployeeForm(request.POST)
+       if form.is_valid():
+        try:
+            form.save()
+            return redirect("/show")
+        except:
+            pass
+    else:
+        form = EmployeeForm()
+    return render(request, "index.html", {'form': form})      
+
+def show(request):
+    employees = Employee.objects.all()      
+    return render(request, "show.html", {'employees': employees}) 
+
+def edit(request, id):     
+    employee = Employee.objects.get(id=id)  
+    return render(request, "edit.html", {'employee': employee})
+def update(request, id):
+    employee = Employee.objects.get(id=id)  
+    form = EmployeeForm(request.POST, instance = employee)  
+    if form.is_valid():
+        form.save() 
+        return redirect('/show')      
+    return render(request, "edit.html", {'employee': employee})
+
+def delete(request, id):
+    employee = Employee.objects.get(id=id)
+    employee.delete()    
+    return redirect("/show")
+
+
+
